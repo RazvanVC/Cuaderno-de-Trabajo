1420e92110ebbdd59bb2aed33b7960e0c495a5a5
RazvanVC <razvanvc@yahoo.com>
2021-10-11 18:41:15 +0200

    Update Ejercicio 4.py

--- Practica 1/Ejercicio 4.py
+++ Practica 1/Ejercicio 4.py
@@ -1,7 +1,7 @@
 print ("4. Escribe un programa en Python que calcule el impuesto que debe pagar un contribuyente a partir de sus ingresos anuales y el número de hijos. \n\
 El impuesto a pagar es un tercio del ingreso imponible, siendo este último igual a los ingresos totales menos una deducción personal de 600€ y otra \n\
 deducción de 60€ por hijo.")
-print ()
+print () 
 
 ingresos_anuales = (float(input("¿Cuales son los ingresos anuales?")))
 print ("Ingresos Anuales: ", ingresos_anuales)
@@ -10,5 +10,3 @@ print ("Número hijos: ", num_hijos)
 imp_imponible = ingresos_anuales-(600+(60*num_hijos))
 imp_pagar = imp_imponible/3
 print ("Impuesto a pagar: ", imp_pagar)
-
-
